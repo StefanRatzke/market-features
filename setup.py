b1a685e079ffe14f856fa0baf96a1766fd4245da
ratzste <stefan.ratzke.ext@deutsche-boerse.com>
2014-11-07 13:47:41 +0100

    add PyPI classification

--- setup.py
+++ setup.py
@@ -7,15 +7,23 @@ setup(
     author_email = '',
     url = 'https://github.com/StefanRatzke/nose-market-features',
     packages=['market_features'],
-    #package_dir={'market_features' : 'market_features'},
     description = 'Create HTML Test Results Report',
     license = '',
-#    py_modules = ['market_features'],
-    entry_points = {
-        'nose.plugins.0.10': [
-            'market_features = market_features:MarketFeatures'
-            ]
-        },
+    entry_points = '''
+        [nose.plugins.0.10]
+        market_features = market_features:MarketFeatures
+        ''',
     package_data={'market_features' : ['market_features.html.jinja']},
-    install_requires = ['Jinja2 >=2.6, < 3.0']
+    install_requires = ['Jinja2 >=2.6, < 3.0'],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+	'Environment :: Console',
+	'Intended Audience :: Developers',
+	'Natural Language :: English',
+	'License :: OSI Approved :: Apache Software License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.7',
+	'Topic :: Software Development :: Testing'
+    ],
 )
