682fc35156d70e354a27383de32ed117e57260fa
Caleb Carvalho <caleb.carvalho@gmail.com>
2015-12-17 11:36:51 +0100

    increase version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='market-features',
-    version='0.3.3',
+    version='0.3.5',
     author='Stefan Ratzke, Caleb Carvalho',
     author_email='stefan-ratzke@t-online.de',
     url='https://github.com/StefanRatzke/market-features',
-    download_url='https://github.com/StefanRatzke/market-features/tarball/0.3.3',
+    download_url='https://github.com/StefanRatzke/market-features/tarball/0.3.5',
     maintainer='Stefan Ratzke, Caleb Carvalho',
     maintainer_email='caleb.carvalho@gmail.com',
     packages=['market_features'],
