09e572aa8b49a43d63b7d294636c422214f4043d
Caleb Carvalho <caleb.carvalho.ext@deutsche-boerse.com>
2016-01-05 18:32:30 +0100

    multiple status validation

--- test/market_error_skip/test_nose_plugin.py
+++ test/market_error_skip/test_nose_plugin.py
@@ -7,20 +7,8 @@ from nose.tools import assert_equals
 
 @attr('test_nose_plugin')
 class TestNosePlugin(unittest2.TestCase):
-    def setUp(self):
-        pass
 
-    def tearDown(self):
-        pass
-
-    def test_one(self):
-        """first test, simulation passing test"""
-        assert_equals(1, 1)
-
+    @skip('skip one and error one')
     def test_one6(self):
         """first test, simulation passing test"""
-        assert_equals(1, 1)
-
-    def test_three(self):
-        """third test, simulation failing test"""
         assert_equals(1, 1)
